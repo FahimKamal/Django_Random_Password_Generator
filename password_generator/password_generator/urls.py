c78131c6bf130905e6c7884d9ed83c4953e55f3e
FahimKamal <fahimkamal63@gmail.com>
2020-06-03 18:02:45 +0600

    part 4 complete.

--- password_generator/password_generator/urls.py
+++ password_generator/password_generator/urls.py
@@ -1,4 +1,4 @@
-"""password_generator-project URL Configuration
+"""password_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from generator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home)
 ]
