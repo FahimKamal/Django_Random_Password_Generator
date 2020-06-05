7640e95718ecdb4b2634989f0882e6162f39a78d
FahimKamal <fahimkamal63@gmail.com>
2020-06-05 11:26:41 +0600

    part 7 complete. Basic password generator is now working.

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+import random
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse('Hello there friend')
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    thepassword = ''
+    characters = list('abcdefghijklmnopqrstwxyz')
+    length = 10
+    for x in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': thepassword})
