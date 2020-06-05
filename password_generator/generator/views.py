411d4971ae96991010e2b02e35fa3beace648883
FahimKamal <fahimkamal63@gmail.com>
2020-06-05 11:58:26 +0600

    part 9 complete. Challenge completed.

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -7,10 +7,25 @@ def home(request):
     return render(request, 'generator/home.html')
 
 
+def about(request):
+    return render(request, 'generator/about.html')
+
+
 def password(request):
-    thepassword = ''
+    the_password = ''
     characters = list('abcdefghijklmnopqrstwxyz')
-    length = 10
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()-=+?`~,.<>'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+    length = int(request.GET.get('length', 12))
+
     for x in range(length):
-        thepassword += random.choice(characters)
-    return render(request, 'generator/password.html', {'password': thepassword})
+        the_password += random.choice(characters)
+    return render(request, 'generator/password.html', {'password': the_password})
